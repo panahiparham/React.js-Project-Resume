17029a1c0739780bb8447e93e28d4136f11d5693
Parham-M-Panahi <panahiparham@gmail.com>
2020-11-15 22:11:16 +0330

    added CSS

--- src/App.js
+++ src/App.js
@@ -8,42 +8,41 @@ function App() {
   return (
     <div className="App">
       <div className="navigation"></div>
-      <div className="home-page">
-        <div className="title">
+
+      <div className="container">
+        <div className="home-page">
           <h1>{data.title}</h1>
-        </div>
-
-        <div className="sub-title">
           <h2>{data.subtitle}</h2>
+          <div className="contact-links">
+            {Object.keys(data.links).map((key) => {
+              return <SocialIcon url={data.links[key]} />;
+            })}
+          </div>
         </div>
-        <div className="contact-links">
-          {Object.keys(data.links).map((key) => {
-            return <SocialIcon url={data.links[key]} />;
-          })}
-        </div>
-      </div>
 
-      <div className="about-page">
-        <h3>{data.sections[0].title}</h3>
-
-        <p>{data.sections[0].items[0].content}</p>
-      </div>
-      <div className="skills-page">
-        <h3>{data.sections[1].title}</h3>
-        <div className="skills-wrapper">
-          {data.sections[1].items.map((skill) => {
-            return (
-              <div className="skill">
-                <div className="image-wrapper">
-                  <img src={skill.content.image} />
-                </div>
-                <div className="skill-title-wrapper">
-                  <h4>{skill.content.title}</h4>
-                </div>
-              </div>
-            );
-          })}
+        <div className="about-page">
+          <h3>{data.sections[0].title}</h3>
+          <p>{data.sections[0].items[0].content}</p>
         </div>
+        <div className="skills-page">
+          <h3>{data.sections[1].title}</h3>
+          <div className="skills-wrapper">
+            {data.sections[1].items.map((skill) => {
+              return (
+                <div className="skill">
+                  <div className="image-wrapper">
+                    <img src={skill.content.image} />
+                  </div>
+                  <div className="skill-title-wrapper">
+                    <h4>{skill.content.title}</h4>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+
+        <footer>&#169; Copyright Parham M. Panahi 2020</footer>
       </div>
     </div>
   );
